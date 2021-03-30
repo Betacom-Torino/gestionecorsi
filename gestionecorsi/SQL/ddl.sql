80b2ddc4ef44b88888fd6514f86ad0698ef533ae
Betacom <Betacom@Betacom-PC109D.station>
2021-03-30 21:15:06 +0200

    insert degli amministratori insert dei docenti

---
+++ gestionecorsi/SQL/ddl.sql
@@ -0,0 +1,17 @@
+/*INSERT AMMINISTRATORE*/
+insert into amministratore values(amministratore_seq,'Mario','Rossi');
+insert into amministratore values(amministratore_seq,'Luca','Bianchi');
+insert into amministratore values(amministratore_seq,'Laura','Verdi');
+insert into amministratore values(amministratore_seq,'Massimo','Neri');
+
+/*INSERT DOCENTI*/
+insert into docente values(docente_seq,'Lorenzo','Giallo','cv/cv1.pdf');
+insert into docente values(docente_seq,'Simone','Arancione','cv/cv2.pdf');
+insert into docente values(docente_seq,'Carla','Viola','cv/cv3.pdf');
+insert into docente values(docente_seq,'Raffaele','Verde','cv/cv4.pdf');
+insert into docente values(docente_seq,'Marco','Rosso','cv/cv5.pdf');
+insert into docente values(docente_seq,'Angela','Giallini','cv/cv6.pdf');
+insert into docente values(docente_seq,'Rosaria','Rosetta','cv/cv7.pdf');
+insert into docente values(docente_seq,'Alberto','Nerone','cv/cv8.pdf');
+insert into docente values(docente_seq,'Claudia','Violetta','cv/cv9.pdf');
+insert into docente values(docente_seq,'GianPiero','Bianchini','cv/cv10.pdf');
