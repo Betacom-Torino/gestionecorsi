b34845dad04affabb257b4d8ddd8beb404408bf0
giorg <giorg@Lupo>
2021-03-31 09:14:33 +0200

    Eliminazione di import inutili! Lupo

--- gestionecorsi/src/test/com/betacom/architecture/dbaccess/DBAccessTest.java
+++ gestionecorsi/src/test/com/betacom/architecture/dbaccess/DBAccessTest.java
@@ -1,8 +1,6 @@
 package test.com.betacom.architecture.dbaccess;
 
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
