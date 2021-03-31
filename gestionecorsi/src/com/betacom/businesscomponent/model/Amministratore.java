f46e04cc3ecf70112628da7d75fb5089149f2cec
giorg <giorg@Lupo>
2021-03-31 08:55:45 +0200

    CTRL+Maiusc+F Lupo

--- gestionecorsi/src/com/betacom/businesscomponent/model/Amministratore.java
+++ gestionecorsi/src/com/betacom/businesscomponent/model/Amministratore.java
@@ -4,23 +4,29 @@ public class Amministratore {
 	private String nomeAdmin;
 	private String cognomeAdmin;
 	private String codAdmin;
+
 	public String getNomeAdmin() {
 		return nomeAdmin;
 	}
+
 	public void setNomeAdmin(String nomeAdmin) {
 		this.nomeAdmin = nomeAdmin;
 	}
+
 	public String getCognomeAdmin() {
 		return cognomeAdmin;
 	}
+
 	public void setCognomeAdmin(String cognomeAdmin) {
 		this.cognomeAdmin = cognomeAdmin;
 	}
+
 	public String getCodAdmin() {
 		return codAdmin;
 	}
+
 	public void setCodAdmin(String codAdmin) {
 		this.codAdmin = codAdmin;
 	}
-	
+
 }
