58b60f3e36af2a04d502533d26312166c822e984
giulia <Betacom@Betacom-PC109D.station>
2021-03-31 23:46:52 +0200

    modifica long

--- gestionecorsi/src/com/betacom/businesscomponent/model/CorsoCorsista.java
+++ gestionecorsi/src/com/betacom/businesscomponent/model/CorsoCorsista.java
@@ -1,57 +1,27 @@
 package com.betacom.businesscomponent.model;
 
 public class CorsoCorsista {
-<<<<<<< HEAD
-	private int codCorsista;
-	private int codCorso;
 
-	public int getCodCorsista() {
-=======
 	private long codCorsista;
 	private long codCorso;
-	
 
-	
 	public long getCodCorsista() {
->>>>>>> branch 'main' of https://github.com/Betacom-Torino/gestionecorsi
 		return codCorsista;
 	}
 
-<<<<<<< HEAD
-	public void setCodCorsista(int codCorsista) {
-=======
-
-
 	public void setCodCorsista(long codCorsista) {
->>>>>>> branch 'main' of https://github.com/Betacom-Torino/gestionecorsi
 		this.codCorsista = codCorsista;
 	}
 
-<<<<<<< HEAD
-	public int getCodCorso() {
-=======
-
-
 	public long getCodCorso() {
->>>>>>> branch 'main' of https://github.com/Betacom-Torino/gestionecorsi
 		return codCorso;
 	}
 
-<<<<<<< HEAD
-	public void setCodCorso(int codCorso) {
-=======
-
 
 	public void setCodCorso(long codCorso) {
->>>>>>> branch 'main' of https://github.com/Betacom-Torino/gestionecorsi
 		this.codCorso = codCorso;
 	}
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> branch 'main' of https://github.com/Betacom-Torino/gestionecorsi
 	@Override
 	public String toString() {
 		return "CorsoCorsista [codCorsista=" + codCorsista + ", codCorso=" + codCorso + "]";
