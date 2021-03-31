832184651924f1693a7e9ae94acb6ac27bc92834
giorg <giorg@Lupo>
2021-03-31 09:07:49 +0200

    Aggiunta test DBAccess! Lupo

---
+++ gestionecorsi/src/test/com/betacom/architecture/dbaccess/DBAccessTest.java
@@ -0,0 +1,32 @@
+package test.com.betacom.architecture.dbaccess;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import com.betacom.architecture.dao.DAOException;
+import com.betacom.architecture.dbaccess.DBAccess;
+
+class DBAccessTest {
+
+	@Test
+	void test() {
+		try {
+			DBAccess.getConnection();
+		}catch(DAOException | IOException | ClassNotFoundException exc) {
+			exc.printStackTrace();
+			fail("Errore nel tentativo di connessione");
+		}finally {
+			try {
+				DBAccess.closeConnection();
+			}catch(DAOException exc) {
+				exc.printStackTrace();
+				fail("Errore nel tentativo di chiusura della connessione");
+			}
+		}
+	}
+
+}
