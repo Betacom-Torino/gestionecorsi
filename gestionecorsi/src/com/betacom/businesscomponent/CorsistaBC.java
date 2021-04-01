9458fbf3c2b9e33e3307ab8526089bd61bcb4999
LuigiDSantos <37957287+LuigiDSantos@users.noreply.github.com>
2021-04-01 09:35:09 +0200

    commit

--- gestionecorsi/src/com/betacom/businesscomponent/CorsistaBC.java
+++ gestionecorsi/src/com/betacom/businesscomponent/CorsistaBC.java
@@ -21,7 +21,7 @@ public class CorsistaBC {
 	public void createOrUpdate(Corsista corsista) throws DAOException, ClassNotFoundException, IOException {
 		idGen = CodGenerator.getIstance();
 		try {
-			if (corsista.getCodiceCor() < CorsistaDAO.getFactory().getNumCorsistiTotali(conn)) {
+			if (corsista.getCodiceCor() <= CorsistaDAO.getFactory().getNumCorsistiTotali(conn)) {
 				CorsistaDAO.getFactory().update(conn, corsista);
 			} else {
 				corsista.setCodiceCor(idGen.getNextCod("corsista"));
